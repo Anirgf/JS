d38df95941fa13dd85dc6344bc4dfdfd8dde3c7d
Anirgf <61605200+Anirgf@users.noreply.github.com>
2022-02-17 22:29:54 -0300

    aula 13 e 14

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+//console.log('o console funcionou corretamente')
+var vei = 61
+console.log (`A velocidade do seu carro é ${vei} km/h`)
+if (vei > 60 ){ //condição simples
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+/*
+var c = 1
+while (c <= 1){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} 
+while (c <= 1
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
